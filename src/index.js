ff466a4f827b90c6578dedb5294f5e9020e956e2
Uros Urosevic <urosevic1986@gmail.com>
2018-03-10 16:51:53 +0800

    add react and webpack DefinePlugin fix

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.info("React")
+import App from './App';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const render = (_App) => {
+  ReactDOM.render(
+    <_App />,
+    document.getElementById('AppContainer')
+  )
+}
+
+render(App);
